c2162480ceface4a3cba423f3b97a3bf5e5753f3
ahmed mohammed <ahmed.c7.mo@gmail.com>
2024-11-08 22:52:16 +0200

    a2

--- src/components/Baner/Baner.jsx
+++ src/components/Baner/Baner.jsx
@@ -5,7 +5,7 @@ export default function Baner() {
         <>
             <div className="baner">
                 <div className="filter"></div>
-                <img src="../../../public/assts/images/iStock-902506410_1200x800px-1200x800.jpg" alt="" />
+                <img src="https://thumbs.dreamstime.com/b/panama-city-skyline-bay-panama-central-america-tw-twilight-91383437.jpg" alt="" />
                 <h1>welcome</h1>
             </div>
         </>
