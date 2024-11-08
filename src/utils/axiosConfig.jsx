b92d20adce65573ca49dc2971d40e4139cdf8036
ahmed mohammed <ahmed.c7.mo@gmail.com>
2024-11-08 22:40:48 +0200

    ed

--- src/utils/axiosConfig.jsx
+++ src/utils/axiosConfig.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const axiosConfig = axios.create(
     {
-        baseURL: 'http://localhost:8000',
+        baseURL: 'https://672e740c229a881691f01a4f.mockapi.io/api',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
